6086692918b6f340c3e583d6c7b3bdd3739ce490
serwindev <93427110+Coder-Serwin@users.noreply.github.com>
2026-04-26 17:11:40 +0530

    Final version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Rank-based-pixel-mapping
-It is an attempt to recreate the website called Obamify -- Using python and pillow.
+It is an attempt to recreate the website called Obamify.
+
+---
+## About the project
+
+This projec was originally started with a motive to recreate the [obamify website](https://obamify.com/) with the help of python and pillow.
+
+It works perfectly -- to be honest. But then, I thought of shipping the whole project into HTML, CSS and JS. Because, i want a portable version, easy-to-use version.
+
+So, Again i started to work on the project with HTML, CSS and JS -- which came pretty good.
+
+---
+
+## How to use?
+
+The good thing is that i have mentioned a whole tutorial in the website itself. 
+
+Because, last time, I was told to improve UX of my app -- so, i wrote the tutorial by myself this time.
+
+Make sure to test the [website]()
+
+---
+
+## Concepts
+
+To be honest, the concepts are so easy and beginner-friendly.
+
+- Step 1: Convert two images into their pixels (First image is the source and Second image is the target image)
+- Step 2: Calculate brightness for each pixel, which is, just the average of RED, GREEN and BLUE of each pixel.
+- Step 3: Then, Sort the array/image based on brightness.
+- Step 4: Use those sorted image, and match pixels of both images.
+- Step 5: With those pixels from both images, take colour from Image ONE and structure (that is, x and y coordinates) from image TWO.
+- Step 6: Draw a new image with new pixels data. 
+- Step 7: You could add Animations or optimize the algorithm by introducing range.
+
+---
+
+## Credits
+
+Made by [Serwin.dev](https://www.instagram.com/serwin.dev/). Consider follow in Instagram 😅
+
+Last time, I was corrected for using AI to write Readme -- this time, I have written the whole ReadME..!! Thanks for those corrections.
+
+---
+
+Submitted for Flavortown, conducted by HackClub.
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const imageInput = document.getElementById("image1");
 const image2Input = document.getElementById("image2");
 const canvas = document.getElementById("mycanvas");
 const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
 
 const invisCanvas = document.getElementById("inviscanvas");
 const ctx_in = invisCanvas.getContext("2d");
@@ -11,8 +12,8 @@ const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
 
 const img1 = new Image();
 const img2 = new Image();
-const i_w = 400;
-const i_h = 400;
+const i_w = 500;
+const i_h = 500;
 
 let t = 0;
 
@@ -33,7 +34,7 @@ function getPixelData(x,y, pixels) {
         g: green,
         b: blue,
         a: alpha,
-        brightness: (red + green + blue) / 3
+        brightness: 0.3*red + 0.59*green + 0.11*blue
     }
 }
 
@@ -92,7 +93,9 @@ function startAnimate() {
 
     ctx.clearRect(0,0, canvas.width, canvas.height);
     Mapping();
-    t += 0.005;
+    if (t>=0.8) {
+        t+=0.01
+    } else {t += 0.005;}
     requestAnimationFrame(startAnimate);
 
 }
@@ -103,14 +106,38 @@ function Mapping() {
     const outImageData = new ImageData(i_w, i_h);
     const buffer = outImageData.data;
     const totalPixels = i_w*i_h;
+    const k = 15;
+    const used = Array.from({length: i_w*i_h}, () => false);
 
     for (let i=0; i<totalPixels; i++) {
+        let best = -1;
+        let bestDis = 1000000;
+        let start = Math.max(0, i-k);
+        let end = Math.min(totalPixels - 1, i+k);
 
         color_A = pixel1[i];
-        pos_B = pixel2[i];
 
-        let x = parseInt(color_A.x + t * (pos_B.x - color_A.x));
-        let y = parseInt(color_A.y + t * (pos_B.y - color_A.y));
+        for (let j=start; j<end; j++) {
+            if (used[j]) {continue;}
+            pos_B = pixel2[j];
+
+            let cd = (color_A.r - pos_B.r)**2 + (color_A.g - pos_B.g)**2 + (color_A.b - pos_B.b)**2;
+
+            if (cd < bestDis) {
+                bestDis = cd;
+                best = j;
+            }
+        }
+
+        if (best == -1) {
+            best = used.indexOf(false);
+        }
+
+        used[best] = true;
+
+        pos_B = pixel2[best];
+        let x = clamp(Math.round(color_A.x + t * (pos_B.x - color_A.x)), 0, i_w);
+        let y = clamp(Math.round(color_A.y + t * (pos_B.y - color_A.y)), 0, i_h);
 
         const index = (y*i_w + x) * 4;
 
@@ -124,6 +151,7 @@ function Mapping() {
 }
 
 function reset() {
+    cancelAnimationFrame(startAnimate);
     pixel1.length = 0;
     pixel2.length = 0;
 
@@ -137,4 +165,16 @@ function reset() {
     image2Input.value = '';
 
     t = 0;
+}
+
+function download() {
+    canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+
+        link.download = "image.png";
+        link.href = url;
+        link.click();
+        URL.revokeObjectURL(url);
+    }, 'image/png')
 }
